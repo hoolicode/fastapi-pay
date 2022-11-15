2965fb1167a678414a1b87ce2ea80fb3c7eff517
SarahLightBourne <SaraLightBourne@gmail.com>
2022-11-15 18:31:09 +0700

    YooKassa Settings

--- core/settings/settings.py
+++ core/settings/settings.py
@@ -6,3 +6,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Server
 API_URL = get_env_var('API_URL')
 API_TOKEN = add_prefix(get_env_var('API_TOKEN'), 'Token')
+
+# YoooKassa
+YOOKASSA_URL = 'https://api.yookassa.ru/v3'
+YOOKASSA_ID = get_env_var('YOOKASSA_ID')
+YOOKASSA_TOKEN = get_env_var('YOOKASSA_TOKEN')
