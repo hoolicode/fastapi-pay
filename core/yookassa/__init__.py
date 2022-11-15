f9926bab64a3c1f439266936fdc5aa75e284112c
SarahLightBourne <SaraLightBourne@gmail.com>
2022-11-15 20:23:55 +0700

    YooKassa payment_create

---
+++ core/yookassa/__init__.py
@@ -0,0 +1,1 @@
+from .payment_create import payment_create
---
+++ core/yookassa/payment_create.py
@@ -0,0 +1,29 @@
+from ..models import Payment
+
+from httpx import Response
+from .session import Session
+from ..settings.settings import YOOKASSA_URL
+
+URL = f'{YOOKASSA_URL}/payments/'
+
+
+async def payment_create(payment: Payment, return_url: str) -> Response:
+  session = Session()
+
+  response = await session.post(
+    URL,
+    headers={'Idempotence-Key': str(payment.idempotence_key)},
+    json={
+      'amount': {
+        'value': payment.price,
+        'currency': 'RUB'
+      },
+      'confirmation': {
+        'type': 'redirect',
+        'return_url': return_url
+      },
+      'capture': True
+    }
+  )
+
+  return response
