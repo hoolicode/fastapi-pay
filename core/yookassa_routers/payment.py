6de36651ba97b4a3f93772b4aaade9dca59d0074
SarahLightBourne <SaraLightBourne@gmail.com>
2022-11-15 21:03:12 +0700

    Log Unregistered Events

--- core/yookassa_routers/payment.py
+++ core/yookassa_routers/payment.py
@@ -1,14 +1,21 @@
 from ..server import payment_update
+
+import logging
 from fastapi import Body, Response
+
+logger = logging.getLogger(__name__)
 
 
 async def payment(data = Body()) -> None:
   payment_id = data['object']['id']
 
-  match data['event']:
+  match (event := data['event']):
 
     case 'payment.succeeded':
       final_price = data['object']['income_amount']['value']
       await payment_update(payment_id, is_pk=False, **{'paid': True, 'final_price': final_price})
+
+    case _:
+      logger.warning('Unregistered event:', event)
 
   return Response()
