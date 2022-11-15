fc6a3deba56c07f70b9a38efe62b245d64db723f
SarahLightBourne <SaraLightBourne@gmail.com>
2022-11-15 20:24:29 +0700

    Update checkout.py

--- core/yookassa_routers/checkout.py
+++ core/yookassa_routers/checkout.py
@@ -1,25 +1,35 @@
-from httpx import ConnectError
-from ..server import payment_create
+from ..models import Payment
+from ..yookassa import payment_create as yookassa_payment_create
 
+from ..server import payment_create as server_payment_create
+from ..server import payment_update as server_payment_update
+
+import logging
 from typing import Dict
+from httpx import ConnectError
+from pydantic import BaseModel, AnyUrl
 from fastapi.responses import ORJSONResponse
-from fastapi import Body, Cookie, HTTPException, status
+from fastapi import Cookie, HTTPException, status
+
+logger = logging.getLogger(__name__)
 
 
-async def checkout(price: int = Body(embed=True), sessionid: str = Cookie()) -> Dict:
+class Data(BaseModel):
+  price: float
+  return_url: AnyUrl
 
-  try:
-    response = await payment_create(price, sessionid)
-  except ConnectError:
-    raise HTTPException(status.HTTP_503_SERVICE_UNAVAILABLE, 'The Server is down')
 
-  match response.status_code:
+async def checkout(data: Data, sessionid: str = Cookie()) -> Dict:
+  response = await server_payment_create(data.price, sessionid)
 
-    case 201:
-      return ORJSONResponse(response.json(), status.HTTP_201_CREATED)
+  payment = Payment(**response.json())
+  response = await yookassa_payment_create(payment, data.return_url)
 
-    case 403:
-      raise HTTPException(status.HTTP_403_FORBIDDEN, response.json())
+  if response.status_code != 200:
+    logger.error(f"YooKassa can't generate a payment\n{response.json()}")
+    raise HTTPException(status.HTTP_503_SERVICE_UNAVAILABLE, 'YooKassa Server is unavailable')
 
-    case _:
-      return ORJSONResponse(response.json(), status.HTTP_400_BAD_REQUEST)
+  response = await server_payment_update(payment, payment_id=response.json()['id'])
+  response_json = response.json()
+
+  return ORJSONResponse({'return_url': response_json['confirmation_url']})
